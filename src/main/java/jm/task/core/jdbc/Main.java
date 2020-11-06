4be0f8dbca7ff58414d67b48ba98c0e37d502a9a
Akromo <zloekrevedko@yandex.ru>
2020-11-06 17:17:06 +0300

    Удалил свой тест конекшна

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,18 +2,19 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 
 public class Main {
     public static void main(String[] args) {
-        UserDao sh = new UserDaoJDBCImpl();
-        sh.createUsersTable();
+        UserDao sh = new UserServiceImpl();
+//        sh.createUsersTable();
         sh.saveUser("Vasya", "Ivanov", (byte) 30);
         sh.saveUser("Ivan", "Vasilev", (byte) 40);
         sh.saveUser("Petr", "TheFirst", (byte) 127);
         sh.saveUser("Java", "Mentor", (byte) -1);
-        sh.getAllUsers().stream().forEach(System.out::println);
-        sh.cleanUsersTable();
-        sh.dropUsersTable();
+//        sh.getAllUsers().stream().forEach(System.out::println);
+//        sh.cleanUsersTable();
+//        sh.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,16 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Hibernate;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.Date;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
 
     }
@@ -22,7 +28,12 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        EntityManager entityManager = Util.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(new User(name, lastName, age));
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        System.out.println(String.format("User с именем – %s добавлен в базу данных", name));
     }
 
     @Override
